7ea1e2f679c996f5dec8830c6530535a570a0e8d
zzzzDenzzzz <12den24@gmail.com>
2023-07-25 16:15:42 +0300

    fix_25-07-2023_16:15

---
+++ templatemo_559_zay_shop 2/templatemo_559_zay_shop/assets/js/photos.js
@@ -0,0 +1,57 @@
+const api = 'https://jsonplaceholder.typicode.com';
+let _photos = [];
+
+async function getPhotos() {
+    try {
+        const response = await fetch(api + '/photos');
+        const photos = await response.json();
+
+        _photos = photos;
+        
+        loadSlides(100);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+(async () => {
+    try {
+        await getPhotos();
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+function loadSlides(countSlides) {
+
+    let contentDiv = document.createElement('div');
+    contentDiv.className = '_content';
+
+    let sliderDiv = document.createElement('div');
+    sliderDiv.className = 'slider autoplay';
+
+    for (let i = 0; i < Math.min(countSlides, _photos.length); i++) {
+        let slideDiv = document.createElement('div');
+        slideDiv.innerHTML= `<img src="${_photos[i].thumbnailUrl}">`;
+
+        sliderDiv.appendChild(slideDiv);
+    }
+
+    contentDiv.appendChild(sliderDiv);
+
+    var parentElement=document.querySelector('#_photos');
+
+    if(parentElement){
+        parentElement.appendChild(contentDiv);
+        
+        $(".autoplay").slick({
+            infinite: true,
+            speed: 500,
+            slidesToShow: 9,
+            slidesToScroll: 2,
+            autoplay: true,
+            autoplaySpeed: 1000
+        });
+    }
+}
